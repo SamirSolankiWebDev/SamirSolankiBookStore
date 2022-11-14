c013b1dca9c64b0c70c0fff7977f75afd9780b4a
W0785689@myscc.ca <W0785689@myscc.ca>
2022-11-14 18:14:43 -0500

    Commit add in the IRepository.cs

--- SamirSolankiBooks.DataAccess/Repository/IRepository/IRepository.cs
+++ SamirSolankiBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -15,5 +15,11 @@ namespace SamirSolankiBooks.DataAccess.Repository.IRepository
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
             string includeProperties = null
             );
+        void Add(T entity); // to add an entiry
+        void Remove(int id);  // to remove an object or category
+        void Remove(T entity); // another way to remove an object
+
+        void RemoveRange(IEnumerable<T> entity);  // remoces a complete range of entities
+
     }
 }
