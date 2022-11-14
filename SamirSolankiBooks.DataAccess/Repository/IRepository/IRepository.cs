e9bf199365bbc6c2acebc181e541d7ad4fca54a6
W0785689@myscc.ca <W0785689@myscc.ca>
2022-11-14 18:10:36 -0500

    Commit Added code in "iRepository.cs"

--- SamirSolankiBooks.DataAccess/Repository/IRepository/IRepository.cs
+++ SamirSolankiBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace SamirSolankiBooks.DataAccess.Repository.IRepository
 {
-    class IRepository
+    public interface IRepository<T> where T : class
     {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
     }
 }
