625744a1133842a9a17ff9aa6b54f53f972af5d7
W0785689@myscc.ca <W0785689@myscc.ca>
2022-11-14 16:25:59 -0500

    commit

--- SamirSolankiBooks.DataAccess/Data/ApplicationDbContext.cs
+++ SamirSolankiBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SamirSolankiBookStore.Data
+namespace SamirSolankiBookStore.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
