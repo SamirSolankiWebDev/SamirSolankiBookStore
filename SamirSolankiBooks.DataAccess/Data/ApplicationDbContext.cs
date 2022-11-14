8fe2177dd9f0834c60d1fe47a7edd08644116b27
W0785689@myscc.ca <W0785689@myscc.ca>
2022-11-14 17:52:05 -0500

    Commit Added Migration

--- SamirSolankiBooks.DataAccess/Data/ApplicationDbContext.cs
+++ SamirSolankiBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using SamirSolankiBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ namespace SamirSolankiBookStore.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
 
