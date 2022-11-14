ca10ae11fa4fff376b4f1509e9009503ced7f956
W0785689@myscc.ca <W0785689@myscc.ca>
2022-11-14 16:43:01 -0500

    Commit

--- SamirSolankiBookStore/Controllers/HomeController.cs
+++ SamirSolankiBookStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using SamirSolankiBookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SamirSolankiBookStore.Models;
 using System;
