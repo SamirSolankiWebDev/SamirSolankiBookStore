a5b1575c7db97cc4c8b51f13e84185f1549e7346
W0785689 <W0785689@myscc.ca>
2022-11-15 09:37:06 -0500

    Commit -Added New Repos

---
+++ SamirSolankiBooks.DataAccess/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using SamirSolankiBooks.DataAccess.Repository.IRepository;
+using SamirSolankiBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamirSolankiBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext
+
+    }
+}
