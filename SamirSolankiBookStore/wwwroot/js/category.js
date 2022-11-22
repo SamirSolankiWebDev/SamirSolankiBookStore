ab8c29c26cbf1ac1fa81c3e336204f2ce991bfa2
W0785689 <W0785689@myscc.ca>
2022-11-22 11:37:27 -0500

    Add code in category.js file

--- SamirSolankiBookStore/wwwroot/js/category.js
+++ SamirSolankiBookStore/wwwroot/js/category.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var dataTable;
 
-// Write your JavaScript code.
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Category/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i>&nbsp;
+                                </a>
+                                <a class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i>&nbsp;
+                                </a>
+                            </div>
+                            `;
+                } 
+            }
+        ]
+    });
+}
