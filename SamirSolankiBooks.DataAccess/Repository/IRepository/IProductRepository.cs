3f193c3c4b8ca37bac347193c89dc70e8c9ef3ab
Samir Solanki <W0785689@myscc.ca>
2022-11-24 02:35:55 -0500

    Added IProductRepository in the application

---
+++ SamirSolankiBooks.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamirSolankiBooks.DataAccess.Repository.IRepository
+{
+   public void Update (Product product)
+    {
+        var objFromDb = _db.Products.FirstOrDefault(s => s.Id == product.Id);
+        if(objFromDb != null)  // save changes if not null
+        {
+            if (product.ImageUrl != null)
+            {
+                objFromDb.ImageUrl = product.ImageUrl; // add the check for Imae=geUrl
+            }
+            objFromDb.Title = product.Title;
+            objFromDb.Description = product.Description;
+            objFromDb.ISBN = product.ISBN;
+            objFromDb.Author = product.Author;
+            objFromDb.ListPrice = product.ListPrice;
+            objFromDb.CategoryId = product.CategoryId;
+            objFromDb.CoverTypeId = product.CoverTypeId;   // all properties of Product object
+
+        }
+    }
+}
