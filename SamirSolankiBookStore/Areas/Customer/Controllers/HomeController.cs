eafb353c4a82e82b3848db605c7eefbee4948875
SamirSolankiWebDev <W0785689@myscc.ca>
2022-12-09 16:05:41 -0500

    Final Changes to Upload on GitHub to Submit The Project

--- SamirSolankiBookStore/Areas/Customer/Controllers/HomeController.cs
+++ SamirSolankiBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SamirSolankiBooks.DataAccess.Repository.IRepository;
+using SamirSolankiBooks.Models;
 
 namespace SamirSolankiBookStore.Controllers
 {
@@ -14,15 +16,18 @@ namespace SamirSolankiBookStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unifOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unifOfWork)
         {
             _logger = logger;
+            _unifOfWork = unifOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unifOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
